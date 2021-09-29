dc0055e01d2fa1f0bdfbcdf9cbed67382f912992
pantherblack34 <89083500+pantherblack34@users.noreply.github.com>
2021-09-29 12:01:23 +0530

    updated html and js

---
+++ area.js
@@ -0,0 +1,19 @@
+const inputVal = document.querySelectorAll('.user-input');
+
+const areaButton = document.querySelector('#areabtn');
+
+const outputArea = document.querySelector('#area-output');
+
+function userInput(a, b){
+    const areaResult = ((a * b)/2);
+    console.log(areaResult);
+    return areaResult;
+}
+
+function calcArea(){
+    const finalAns = userInput(Number(inputVal[0].value), Number(inputVal[1].value));
+
+    outputArea.innerText = "The Area of the triangle is " + finalAns;
+}
+
+areaButton.addEventListener('click', calcArea);
--- hypotenus.js
+++ hypotenus.js
@@ -4,18 +4,19 @@ const hypoButton = document.querySelector('#hypotenusbtn');
 
 const output = document.querySelector('#output-text');
 
-function sumOfSquare(a, b){
+function sumOfSquare(a, b) {
     const userValue = Math.pow(a, 2) + Math.pow(b, 2);
-    
+
     const hypoValue = Math.sqrt(userValue);
     return hypoValue;
 }
 
 
 
-function hypotenusCalc(){
+function hypotenusCalc() {
 
-const result = sumOfSquare(Number(inputArea[0].value), Number(inputArea[1].value));
+    const result = sumOfSquare(Number(inputArea[0].value), Number(inputArea[1].value));
+
     output.innerText = "The hypotenus of the triangle is " + result;
 }
 
