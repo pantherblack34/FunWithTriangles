8973f4e0bac4163cb4dafbb108fafd80b12cfda3
pantherblack34 <89083500+pantherblack34@users.noreply.github.com>
2021-09-29 11:18:19 +0530

    updated hypotenus html and js

---
+++ hypotenus.js
@@ -0,0 +1,23 @@
+const inputArea = document.querySelectorAll('.input-text');
+
+const hypoButton = document.querySelector('#hypotenusbtn');
+
+const output = document.querySelector('#output-text');
+
+function sumOfSquare(a, b){
+    const userValue = Math.pow(a, 2) + Math.pow(b, 2);
+    
+    const hypoValue = Math.sqrt(userValue);
+    return hypoValue;
+}
+
+
+
+function hypotenusCalc(){
+
+const result = sumOfSquare(Number(inputArea[0].value), Number(inputArea[1].value));
+    output.innerText = "The hypotenus of the triangle is " + result;
+}
+
+
+hypoButton.addEventListener('click', hypotenusCalc);
